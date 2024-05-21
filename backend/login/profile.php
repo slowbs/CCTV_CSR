fdaae906b5bda46e0f0040596703a1d32592de1b
slowbs <poison_word@hotmail.com>
2024-05-21 15:35:15 +0700

    profile ได้แล้ว

--- backend/login/profile.php
+++ backend/login/profile.php
@@ -1,31 +1,17 @@
 <?php
 
-if (isset($_SESSION['login'])) {
-    if (empty($_SESSION['login'])) {
-        http_response_code(400);
-        exit(json_encode([
-            'message' => 'Session is required'
-        ]));
-    }
 
+if (isset($_SESSION['login']) && !empty($_SESSION['login'])) {
     $user_id = $_SESSION['login'];
-    $sql = "SELECT * FROM user WHERE user_id = '$user_id'";
-    $query = mysqli_query($conn, $sql);
-    $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
-
-    echo json_encode([
+    $query = mysqli_query($conn, "SELECT * FROM user where user_id = '$user_id'");
+    $result = mysqli_fetch_assoc($query);
+    exit(json_encode([
         'message' => 'Profile API',
-        // 'result' => $result,
         'session' => $result
-    ], JSON_UNESCAPED_UNICODE);
+    ], JSON_UNESCAPED_UNICODE));
 } else {
     http_response_code(400);
     exit(json_encode([
-        'message' => 'The Session is invalid'
+        'message' => 'Cannot Access this Page caue Session is required'
     ]));
 }
-
-
-// echo json_encode(
-//                     $result
-// , JSON_UNESCAPED_UNICODE);
