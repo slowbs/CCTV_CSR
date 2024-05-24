8137cd70609e6c815016ac803e092f95c54e060d
slowbs <poison_word@hotmail.com>
2024-05-24 08:50:17 +0700

    เอา @Input ออก
    เอา @Input Component ออก

--- frontend/src/app/shareds/components/auth-sidebar/auth-sidebar.component.ts
+++ frontend/src/app/shareds/components/auth-sidebar/auth-sidebar.component.ts
@@ -1,25 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AppURL } from '../../../app.url';
 import { AuthenticationURL } from '../../../authentication/authentication.url';
-import { AuthNavbarComponent } from '../auth-navbar/auth-navbar.component';
-import { ISession } from '../../cctv.service';
 
 @Component({
   selector: 'app-auth-sidebar',
   templateUrl: './auth-sidebar.component.html',
   styleUrl: './auth-sidebar.component.css'
 })
-export class AuthSidebarComponent implements OnInit{
-
-  @Input('AuthNavbarComp') AuthNavbarComp: AuthNavbarComponent
-
-  // public profileItem: ISession.Session
+export class AuthSidebarComponent {
 
   AppUrl = AppURL
   AuthUrl = AuthenticationURL
-
-  ngOnInit(): void {
-    this.AuthNavbarComp.get_profile()
-  }
 
 }
