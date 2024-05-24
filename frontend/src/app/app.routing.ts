73b816f2de702ebd66b3113348bf2804daf9134a
slowbs <poison_word@hotmail.com>
2024-05-24 09:05:21 +0700

    ng g guard
    guard work with boolean

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -2,14 +2,19 @@ import { Routes, RouterModule } from "@angular/router";
 import { AppURL } from "./app.url";
 import { LoginComponent } from "./components/login/login.component";
 import { RegisterComponent } from "./components/register/register.component";
+import { authenticationGuard } from "./guards/authentication.guard";
 
 const RouteLists: Routes = [
     { path: '', redirectTo: AppURL.Login, pathMatch: 'full' },
     { path: AppURL.Login, component: LoginComponent },
     { path: AppURL.Register, component: RegisterComponent },
     // { path: AppURL.Authen, loadChildren: 'authentication/authentication.module#AuthenticationModule'}
-    { path: AppURL.Authen, loadChildren: () => import('./authentication/authentication.module').then(x => x.AuthenticationModule)},
-    
+    {
+        path: AppURL.Authen,
+        loadChildren: () => import('./authentication/authentication.module').then(x => x.AuthenticationModule),
+        canActivate: [authenticationGuard]
+    },
+
 ];
 
 export const AppRouting = RouterModule.forRoot(RouteLists);
