b59d0f38a2b01c7f54c7f14a2b29700d1ebea900
slowbs <poison_word@hotmail.com>
2024-05-24 08:48:26 +0700

    sidebar เรียกใช้ function navbar
    Sidebar เรียกใช้ Function ของ Navbar แต่แสดงบน html ไม่ได้

--- frontend/src/app/shareds/components/auth-sidebar/auth-sidebar.component.ts
+++ frontend/src/app/shareds/components/auth-sidebar/auth-sidebar.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { AppURL } from '../../../app.url';
 import { AuthenticationURL } from '../../../authentication/authentication.url';
+import { AuthNavbarComponent } from '../auth-navbar/auth-navbar.component';
+import { ISession } from '../../cctv.service';
 
 @Component({
   selector: 'app-auth-sidebar',
   templateUrl: './auth-sidebar.component.html',
   styleUrl: './auth-sidebar.component.css'
 })
-export class AuthSidebarComponent {
+export class AuthSidebarComponent implements OnInit{
+
+  @Input('AuthNavbarComp') AuthNavbarComp: AuthNavbarComponent
+
+  // public profileItem: ISession.Session
 
   AppUrl = AppURL
   AuthUrl = AuthenticationURL
+
+  ngOnInit(): void {
+    this.AuthNavbarComp.get_profile()
+  }
 
 }
