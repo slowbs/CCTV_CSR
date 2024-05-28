d528b48a0a3ae917503bfcc3804cced580c9aae7
slowbs <poison_word@hotmail.com>
2024-05-28 13:44:56 +0700

    เรียงลำดับกล้อง ตาม floor

--- backend/cctvs/get.php
+++ backend/cctvs/get.php
@@ -3,11 +3,11 @@
 // $sql = "SELECT * FROM cctv WHERE '1' 
 // order by date_created desc limit 4";
 $sql = "SELECT id, durable_no, durable_name, brand, location, floor.floor_name as floor, status.status_name as status, monitor, ip, ping,
-cctv.date_updated, cctv.status as status_id, cctv.floor as floor_id FROM cctv
+cctv.date_updated, cctv.status as status_id, cctv.floor as floor_id, floor.order as floor_order FROM cctv
 left join floor on cctv.floor = floor.floor_id
 left join status on cctv.status = status.status_id
 where 1
-order by floor_id";
+order by floor_order";
 $query = mysqli_query($conn, $sql);
 $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
 
