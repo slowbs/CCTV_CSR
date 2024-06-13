c36b0e7717f6f71e5aa6258e47b886a1cf6e27b8
slowbs <poison_word@hotmail.com>
2024-06-13 15:59:52 +0700

    before search

--- backend/status/get.php
+++ backend/status/get.php
@@ -1,13 +1,13 @@
 <?php
 
-// $sql = "SELECT * FROM status WHERE '1'";
-// $query = mysqli_query($conn, $sql);
-// $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
+$sql = "SELECT * FROM status WHERE '1'";
+$query = mysqli_query($conn, $sql);
+$result = mysqli_fetch_all($query, MYSQLI_ASSOC);
 
-// echo json_encode([
-//     'message' => 'test ทดสอบ',
-//     'result' => $result
-// ], JSON_UNESCAPED_UNICODE);
+echo json_encode([
+    'message' => 'test ทดสอบ',
+    'result' => $result
+], JSON_UNESCAPED_UNICODE);
 
 // echo json_encode(
 //                     $result
@@ -56,32 +56,32 @@
 //     echo "0 results";
 // }
 
-$count_ping_online = 0;
-$count_ping_offline = 0;
+// $count_ping_online = 0;
+// $count_ping_offline = 0;
 
 
-for ($i = 101; $i < 227; $i++) {
-    $url = '172.16.0.' . $i;
-    echo $url;
-    $ch = curl_init($url);
-    // $timeout = '0';
-    curl_setopt($ch, CURLOPT_NOBODY, true);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    // curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-    curl_setopt($ch, CURLOPT_TIMEOUT_MS, 100);
-    curl_exec($ch);
-    $retcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
-    if (200 == $retcode) {
-        // All's well
-        echo "yes \n";
-        $count_ping_online += 1;
-    } else {
-        // not so much
-        echo "no \n";
-        $count_ping_offline += 1;
-    }
-}
+// for ($i = 101; $i < 227; $i++) {
+//     $url = '172.16.0.' . $i;
+//     echo $url;
+//     $ch = curl_init($url);
+//     // $timeout = '0';
+//     curl_setopt($ch, CURLOPT_NOBODY, true);
+//     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+//     // curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+//     curl_setopt($ch, CURLOPT_TIMEOUT_MS, 100);
+//     curl_exec($ch);
+//     $retcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+//     curl_close($ch);
+//     if (200 == $retcode) {
+//         // All's well
+//         echo "yes \n";
+//         $count_ping_online += 1;
+//     } else {
+//         // not so much
+//         echo "no \n";
+//         $count_ping_offline += 1;
+//     }
+// }
 
-echo "Online = " . $count_ping_online . "\n";
-echo "Offline = " . $count_ping_offline . "\n";
+// echo "Online = " . $count_ping_online . "\n";
+// echo "Offline = " . $count_ping_offline . "\n";
