560285564c7fbf97db034f15f27e369c02057430
slowbs <poison_word@hotmail.com>
2024-11-05 00:37:09 +0700

    แก้ cursor card และสามารถ hover ได้แล้ว

--- frontend/src/app/authentication/components/dashboard/dashboard.component.ts
+++ frontend/src/app/authentication/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AppURL } from '../../../app.url'; // แก้ไข path ใหตรงกับที่ตั้งจริงของ AppUrl
+import { AuthenticationURL } from '../../authentication.url'; // แก้ไข path ใหตรงกับที่ตั้งจริงของ AppUrl
 
 @Component({
   selector: 'app-dashboard',
@@ -6,12 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  constructor() {
-  }
+  // ประกาศตัวแปรที่ต้องการใช้
+  AppUrl = AppURL;
+  AuthUrl = AuthenticationURL;
+
+  constructor() {}
 
   ngOnInit(): void {
-
+    // สามารถเรียกใช้ฟังก์ชันหรือข้อมูลอื่น ๆ ได้ที่นี่
   }
-
-
 }
