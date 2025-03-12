0c1f75a66dd93e372693d89cbe8875b8b60ee6c4
slowbs <poison_word@hotmail.com>
2025-03-12 11:26:17 +0700

    เพิ่ม export pdf ได้แล้ว

--- frontend/src/app/authentication/components/check-list/check-list.component.ts
+++ frontend/src/app/authentication/components/check-list/check-list.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CctvService } from '../../../shareds/cctv.service';
+import * as pdfMake from "pdfmake/build/pdfmake";
+import * as pdfFonts from '../../../../assets/font/vfs_fonts';
+import { DatePipe } from '@angular/common';
 
+(<any>pdfMake).addVirtualFileSystem(pdfFonts);
+
+pdfMake.addFonts({
+  Sarabun: {
+    normal: 'Sarabun-Regular.ttf',
+    bold: 'Sarabun-Bold.ttf',
+  },
+});
 
 @Component({
   selector: 'app-check-list',
@@ -11,7 +22,7 @@ export class CheckListComponent implements OnInit {
   selectedMonth: string = '';
   checkListItems: any[] = [];
 
-  constructor(private cctvService: CctvService) { }
+  constructor(private cctvService: CctvService, private datePipe: DatePipe) { }
 
   ngOnInit(): void {
     this.selectedMonth = this.getLastMonth();
@@ -70,5 +81,82 @@ export class CheckListComponent implements OnInit {
     this.cctvService.saveChecklistLogs(data).subscribe((result) => {
       console.log(result);
     });
+  }
+
+  //generate pdf
+  generatePDF(): void {
+    const documentDefinition = {
+      defaultStyle: {
+        font: 'Sarabun',
+        fontSize: 8
+      },
+      content: [
+        { text: 'รายงานการตรวจสอบระบบสำรองข้อมูล', style: 'header', alignment: 'center', margin: [0, 0, 0, 5] },
+        {
+          text: this.selectedMonth ? `ข้อมูลสำหรับเดือน: ${this.formatDateForDisplay(this.selectedMonth)}` : '',
+          alignment: 'center',
+          margin: [0, 0, 0, 10]
+        },
+        this.getTableContent()
+      ],
+      styles: {
+        header: {
+          fontSize: 14,
+          bold: true
+        },
+        tableHeader: {
+          bold: true,
+          fillColor: '#2980b9',
+          color: 'white'
+        }
+      }
+    };
+
+    pdfMake.createPdf(documentDefinition).download('checklist.pdf');
+  }
+
+  //สร้างเนื้อหาตาราง
+  getTableContent() {
+    const body: any[][] = [];
+
+    // สร้าง header row
+    body.push([
+      { text: 'ลำดับที่', style: 'tableHeader', alignment: 'center' },
+      { text: 'รายการตรวจสอบ', style: 'tableHeader', alignment: 'center' },
+      { text: 'กำหนดเวลา', style: 'tableHeader', alignment: 'center' },
+      { text: 'สถานะ', style: 'tableHeader', alignment: 'center' },
+      { text: 'หมายเหตุ', style: 'tableHeader', alignment: 'center' }
+    ]);
+
+    // วนลูปข้อมูล
+    this.checkListItems.forEach((item, index) => {
+      body.push([
+        { text: (index + 1).toString(), alignment: 'center' },
+        { text: item.task },
+        { text: item.schedule },
+        { text: item.status, alignment: 'center' },
+        { text: item.comment || '' }
+      ]);
+    });
+
+    return {
+      table: {
+        headerRows: 1,
+        widths: ['auto', '*', 'auto', 'auto', '*'],
+        body: body
+      }
+    };
+  }
+
+  formatDateForDisplay(input: string): string {
+    // input format yyyy-MM
+    const [year, month] = input.split('-').map(Number);
+    const date = new Date(year, month - 1, 1);
+    const transformedDate = this.datePipe.transform(date, 'MMMM yyyy', 'th-TH')
+    if (transformedDate) {
+      const yearInBuddhistEra = date.getFullYear() + 543;
+      return transformedDate.replace(date.getFullYear().toString(), yearInBuddhistEra.toString());
+    }
+    return "";
   }
 }
