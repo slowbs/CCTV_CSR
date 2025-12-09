c53859e7a769112a24fbb9b240a93c83d0b8cc68
slowbs <poison_word@hotmail.com>
2025-12-09 15:56:29 +0700

    change notify to group

--- python/telegram_utils.py
+++ python/telegram_utils.py
@@ -1,7 +1,8 @@
 import requests
 
 TELEGRAM_BOT_TOKEN = '7725475514:AAESQ0vZWNyphDaa630sQaaLgvl7dMkCvuo'
-TELEGRAM_CHAT_ID = '6334503369'
+# TELEGRAM_CHAT_ID = '6334503369'
+TELEGRAM_CHAT_ID = '-5011497123'
 
 def send_telegram_message(message):
     url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
